00c5e7898b04e31454962cee93699da99baa29e4
Kalle Ilves <kalleilv@gmail.com>
2021-12-03 10:12:57 +0200

    check fragments

--- src/utils.py
+++ src/utils.py
@@ -1,10 +1,25 @@
 import requests
 from functools import lru_cache
 from urllib.parse import urlparse
+from pyquery import PyQuery
+from urllib.parse import unquote
 
 
 def get_url_without_fragment(url):
     return url.split('#')[0]
+
+
+def get_url_fragment(url):
+    parts = url.split('#')
+
+    return parts[1] if len(parts) > 1 else None
+
+
+def html_has_element_with_id(html, id):
+    query = PyQuery(html)
+    normalized_id = unquote(id)
+
+    return len(query(f'#{normalized_id},[href="#{normalized_id}"]')) > 0
 
 
 def get_base_url(url):
@@ -14,16 +29,24 @@ def get_base_url(url):
 
 
 @lru_cache(maxsize=200)
-def cached_url_is_broken(url):
+def get_url_response(url):
     try:
-        respose = requests.get(url, timeout=10)
-
-        return respose.status_code != requests.codes.ok
+        return requests.get(url, timeout=10)
     except (requests.exceptions.ConnectionError, requests.exceptions.Timeout):
-        return True
+        return None
 
 
 def url_is_broken(url):
     normalized_url = get_url_without_fragment(url)
 
-    return cached_url_is_broken(normalized_url)
+    response = get_url_response(normalized_url)
+
+    if response is None or response.status_code != requests.codes.ok:
+        return True
+
+    fragment = get_url_fragment(url)
+
+    if fragment is None or fragment.startswith('/'):
+        return False
+
+    return not html_has_element_with_id(response.text, fragment)
